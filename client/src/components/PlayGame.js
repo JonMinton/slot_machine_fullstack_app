766000a98326be7f782c10ce875bcb2fdb5443cc
Jon Minton <jon.will.minton@gmail.com>
2023-05-03 13:35:50 +0100

    reimplement Paul's knob

--- client/src/components/PlayGame.js
+++ client/src/components/PlayGame.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './PlayGame.css'
 
-const PlayGame = ({ handlePlay }) => {
 
-    const handleClick = () => {
+const PlayGame = ({ handlePlay }) => {
+    const handleContainer = useRef(null)
+    const knob = useRef(null)
+
+    const PullKnob = () => {
+        handleContainer.current.classList.add('pulled');
+        setTimeout(() => {
+            handleContainer.current.classList.remove('pulled');
+        }, 1200)
         handlePlay()
     }
 
-    return (
-        <div className="PlayGame">
-            <button onClick={handleClick}><strong>Play!</strong></button>
+
+    return (<div className="Handle">
+        <div className="content-container" ref={handleContainer}>
+            <div className="base"></div>
+            <div className="handle-overlay"></div>
+            <div className="knob" ref={knob} onClick={PullKnob}></div>
         </div>
-    );
+
+
+    </div>);
 }
+
+
 
 export default PlayGame;
