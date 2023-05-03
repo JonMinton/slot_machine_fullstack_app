abc65c78d1f22e1ea598460b0791aec114279328
Jon Minton <jon.will.minton@gmail.com>
2023-05-03 12:22:19 +0100

    made the knob shinier and everything else look a bit shittier (on purpose)

--- client/src/components/StreakBox.js
+++ client/src/components/StreakBox.js
@@ -2,11 +2,23 @@ import React from 'react';
 import './StreakBox.css'
 
 const StreakBox = ({ winStreak, loseStreak }) => {
+
+    // The idea of this is that the display will be two one digit wheels, so always 
+    // shown to places, and a different colour compared with surrounding display
+    function zeroPad(numberStr) {
+        console.log(numberStr)
+        var numString = numberStr.toString();          
+        console.log(numString)
+        var paddedStr = numString.padStart(2, "0")
+        console.log(paddedStr)
+        return paddedStr;
+      }
+      
+      
     return (
         <div className="StreakBox">
-
-            <span> Streak - Win:{winStreak}</span>
-            <span>Lose:{loseStreak}</span>
+            <span> Streak - Win: <span className="StreakBox-Number">{zeroPad(winStreak)}</span> </span>
+            <span>Lose: <span className="StreakBox-Number">{zeroPad(loseStreak)}</span></span>
         </div>
     );
 }
