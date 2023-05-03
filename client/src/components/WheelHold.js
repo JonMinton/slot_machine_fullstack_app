15680490e1e3790f523ed637d276183abe85f06e
Jon Minton <jon.will.minton@gmail.com>
2023-05-03 20:23:56 +0100

    add 'burn' to displays

--- client/src/components/WheelHold.js
+++ client/src/components/WheelHold.js
@@ -1,11 +1,20 @@
+
+import useSound from 'use-sound';
+import pressButtonSound from '../sounds/buttonPress.wav'
+
 import React from 'react';
 import './WheelHold.css'
 
 
 const WheelHold = ({hold, toggleHold, preventHold}) => {
 
+    const [makePressButtonSound] = useSound(
+        pressButtonSound
+    )
+
     const handleClick = () => {
         console.log("WheelHold toggled")
+        makePressButtonSound()
         if (!preventHold) {
             toggleHold()
         }
